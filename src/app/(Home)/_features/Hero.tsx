6fe01beae303626d20a59f0a34e232285d083cf6
Burak Emre Küpçüoğlu <emrekupcuoglu@gmail.com>
2025-04-16 10:08:10 +0300

    updated the wave animation

--- src/app/(Home)/_features/Hero.tsx
+++ src/app/(Home)/_features/Hero.tsx
@@ -4,25 +4,19 @@ import sky from "@/../public/mountain-sky.jpg";
 // import sky from "@/../public/gargantuan.png";
 
 import * as motion from "motion/react-client";
+// import { motion } from "motion/react";
 
 function Hero({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative">
+    <div className="relative overflow-hidden">
       <div className="relative -z-50 aspect-video w-full">
         <div className="py-36">{children}</div>
         <Image src={sky} className="-z-50" alt="sky" fill />
       </div>
       <div className="relative left-1/2 w-[6400px] -translate-x-1/2">
-        <motion.div
-          animate={{ translateX: "-25%" }}
-          transition={{
-            duration: 20,
-            repeat: Infinity,
-            ease: [0.36, 0.45, 0.63, 0.53],
-          }}
-          className="wave"
-        ></motion.div>
-        {/* <div className="ocean"></div> */}
+        <div className="wave"></div>
+
+        <div className="wave-2"></div>
       </div>
     </div>
   );
